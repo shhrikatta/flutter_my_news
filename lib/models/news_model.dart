32efb08561a7f0325062b724418b16e566c3f0f9
charan <shhrikatta@gmail.com>
2021-11-05 00:25:13 +0530

    creation of api json model class

---
+++ lib/models/news_model.dart
@@ -0,0 +1,35 @@
+class NewsModel {
+  late final int id; // The item's unique id.
+  late final bool deleted; // true if the item is deleted.
+  late final String
+      type; // The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+  late final String by; // The username of the item's author.
+  late final int time; // Creation date of the item, in Unix Time.
+  late final String text; // The comment, story or poll text. HTML.
+  late final bool dead; // true if the item is dead.
+  late final String
+      parent; // The comment's parent: either another comment or the relevant story.
+  late final List<dynamic>
+      kids; // The ids of the item's comments, in ranked display order.
+  late final String url; // The URL of the story.
+  late final int score; // The story's score, or the votes for a pollopt.
+  late final String title; // The title of the story, poll or job. HTML.
+  late final int
+      descendants; // In the case of stories or polls, the total comment count.
+
+  NewsModel.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    deleted = parsedJson['deleted'];
+    type = parsedJson['type'];
+    by = parsedJson['by'];
+    time = parsedJson['time'];
+    text = parsedJson['text'];
+    dead = parsedJson['dead'];
+    parent = parsedJson['parent'];
+    kids = parsedJson['kids'];
+    url = parsedJson['url'];
+    score = parsedJson['score'];
+    title = parsedJson['title'];
+    descendants = parsedJson['descendants'];
+  }
+}
