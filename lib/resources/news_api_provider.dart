26f34f7584502a982c65c159d763468731389a79
charan <shhrikatta@gmail.com>
2021-11-05 00:27:16 +0530

    provider class to make api calls

---
+++ lib/resources/news_api_provider.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter_my_news/models/news_model.dart';
+import 'package:http/http.dart' show Client;
+
+const _baseUrl = 'https://hacker-news.firebaseio.com/v0';
+
+class NewsApiProvider {
+  Client client = Client();
+
+  fetchTopIds() async {
+    final Uri uri = Uri.parse('$_baseUrl/topstories.json');
+    var res = await client.get(uri);
+    final ids = jsonDecode(res.body);
+
+    return ids;
+  }
+
+  fetchNewsStories(int id) async {
+    final Uri uri = Uri.parse('$_baseUrl/item/$id.json');
+    var res = await client.get(uri);
+
+    return NewsModel.fromJson(jsonDecode(res.body));
+  }
+}
