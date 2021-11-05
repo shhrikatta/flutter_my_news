2c80b518f8aaa390025640286b0c4ea571c33285
charan <shhrikatta@gmail.com>
2021-11-05 22:17:21 +0530

    creation of provider

---
+++ lib/src/providers/news_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_my_news/src/blocs/news_bloc.dart';
+
+class NewsProvider extends InheritedWidget {
+  final NewsBloc newsBloc;
+
+  NewsProvider({Key? key, required Widget child})
+      : newsBloc = NewsBloc(),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(_) => true;
+
+  static NewsBloc of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType() as NewsProvider)
+        .newsBloc;
+  }
+}
